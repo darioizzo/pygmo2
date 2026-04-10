2142d53e7167f85b1737b33ddb0a6c9207e1e5fa
Dario Izzo <dario.izzo@gmail.com>
2026-04-10 20:38:45 +0200

    no parallel compilation

--- tools/gha_manylinux.sh
+++ tools/gha_manylinux.sh
@@ -93,7 +93,7 @@ cmake -DBoost_NO_BOOST_CMAKE=ON \
 	-DPAGMO_WITH_IPOPT=yes \
 	-DPAGMO_ENABLE_IPO=ON \
 	-DCMAKE_BUILD_TYPE=Release ../
-cmake --build . --target install --parallel 2
+cmake --build . --target install --parallel 1
 
 # Configure and install pygmo against the selected Python interpreter.
 cd "${GITHUB_WORKSPACE}"
@@ -104,7 +104,7 @@ cmake -DBoost_NO_BOOST_CMAKE=ON \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DPYGMO_ENABLE_IPO=ON \
 	-DPython3_EXECUTABLE="${PYBIN}/python" ../
-cmake --build . --target install --parallel 2
+cmake --build . --target install --parallel 1
 
 # Build wheel from the wheel/ packaging directory and repair it for manylinux.
 cd wheel
