eaec37c3d9a0f9e341adda793d2d10fc37e8440e
Dario Izzo <dario.izzo@gmail.com>
2026-04-10 18:41:54 +0200

    trying fixes for faiing CI

--- tools/gha_windows.ps1
+++ tools/gha_windows.ps1
@@ -1,5 +1,14 @@
-
 # Powershell script
+$ErrorActionPreference = "Stop"
+
+
+$prefixRoot = $env:CONDA_PREFIX
+$prefixLibrary = Join-Path $prefixRoot "Library"
+$pagmoConfigDir = Join-Path $prefixLibrary "lib\cmake\pagmo"
+
+Write-Host "CONDA_PREFIX: $prefixRoot"
+Write-Host "Library prefix: $prefixLibrary"
+
 conda config --set always_yes yes
 conda config --add channels conda-forge
 conda config --set channel_priority strict
@@ -10,11 +19,9 @@ git clone https://github.com/esa/pagmo2.git
 cd pagmo2
 mkdir build
 cd build
-
-cmake `
-    -G "Visual Studio 17 2022" -A x64 `
-    -DCMAKE_PREFIX_PATH=$env:CONDA_PREFIX `
-    -DCMAKE_INSTALL_PREFIX=$env:CONDA_PREFIX `
+cmake -G "Visual Studio 17 2022" -A x64 `
+    -DCMAKE_PREFIX_PATH="$prefixRoot;$prefixLibrary" `
+    -DCMAKE_INSTALL_PREFIX="$prefixLibrary" `
     -DBoost_NO_BOOST_CMAKE=ON `
     -DPAGMO_WITH_EIGEN3=ON `
     -DPAGMO_WITH_IPOPT=ON `
@@ -26,17 +33,24 @@ cmake --build . --config Release --target install
 cd ../
 cd ../
 
-mkdir build
+if (Test-Path build) { Remove-Item -Recurse -Force build }
+New-Item -ItemType Directory -Path build | Out-Null
 cd build
-cmake `
-    -G "Visual Studio 17 2022" -A x64 `
-    -DCMAKE_PREFIX_PATH=$env:CONDA_PREFIX `
-    -DCMAKE_INSTALL_PREFIX=$env:CONDA_PREFIX `
+
+if (-not (Test-Path $pagmoConfigDir)) {
+    throw "pagmo config directory not found: $pagmoConfigDir"
+}
+
+cmake -G "Visual Studio 17 2022" -A x64 `
+    -DCMAKE_PREFIX_PATH="$prefixRoot;$prefixLibrary" `
+    -DCMAKE_INSTALL_PREFIX="$prefixRoot" `
+    -Dpagmo_DIR="$pagmoConfigDir" `
+    -DPython3_EXECUTABLE="$prefixRoot\\python.exe" `
     -DBoost_NO_BOOST_CMAKE=ON `
     -DPYGMO_ENABLE_IPO=yes `
     ..
 
 cmake --build . --config Release --target install
 
-cd c:\
-python -c "import pygmo; pygmo.test.run_test_suite(1); pygmo.mp_island.shutdown_pool(); pygmo.mp_bfe.shutdown_pool()"
+cd $prefixRoot
+& "$prefixRoot\python.exe" -c "import pygmo; pygmo.test.run_test_suite(1); pygmo.mp_island.shutdown_pool(); pygmo.mp_bfe.shutdown_pool()"
